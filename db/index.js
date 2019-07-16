6587edf4044bc8713c926c91aa17c682132e20d1
deadcode <josephsonsjoseph@gmail.com>
2019-07-16 09:05:51 -0700

    "Updated database connection details"

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'aa1gdisvq3uw7kb.cf4rswsctn5j.us-west-1.rds.amazonaws.com',
+  host: 'aa16d3d97x7shud.cqszfcm8ylk3.us-west-2.rds.amazonaws.com',
   user: 'ebroot',
   password: 's1c4o8t7t',
   database: 'sosaCloud',
