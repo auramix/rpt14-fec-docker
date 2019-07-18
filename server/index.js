3cffaff51ea5efcc919e5d12c3c02045dba933bd
deadcode <josephsonsjoseph@gmail.com>
2019-07-17 19:22:24 -0700

    "Added trackId as request param handling"

--- server/index.js
+++ server/index.js
@@ -3,17 +3,15 @@ const app = express();
 const api = require('./api.js');
 
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use('/', express.static(__dirname + '/../client/dist'));
+app.use('/:id', express.static(__dirname + '/../client/dist'));
 
 // Retrieves track info from db
 app.get(['/api/track/:id', '/api/track'], (req, res) => {
   let id = req.params.id;
-  if (id === -1) {
+  if (id === -1 || id === undefined) {
     id = Math.ceil(Math.random() * 100);
-  } else if (id === undefined) {
-    id = 5;
   }
-  console.log('BaseURL/OriginalURL/id: ', req.baseUrl, req.originalUrl, id);
   api.getRelatedTracks(id, (err, results) => {
     if (err) {
       res.status(404).json(err);
@@ -28,7 +26,6 @@ app.get('/api/user/:user_name', (req, res) => {
   let userName = req.params.user_name;
   api.getUserInfo(userName, (err, results) => {
     if (err) {
-      console.log('db Query Error: ', err)
       res.status(404).json(err);
     } else {
       res.status(200).json(results);
@@ -41,7 +38,6 @@ app.get('/api/track/likes/:track_id', (req, res) => {
   let trackId = req.params.track_id;
   api.getLikesInfo(trackId, (err, results) => {
     if (err) {
-      console.log('db Query Error: ', err)
       res.status(404).json(err);
     } else {
       res.status(200).json(results);
